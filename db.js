65c72b1100ccbd6dca698d6757883661fe5f9340
liguangyi <252438823@qq.com>
2017-08-12 00:16:26 +0800

    update

--- db.js
+++ db.js
@@ -1,40 +1,77 @@
 const mysql = require('mysql');
 const constants = require('./constants.js');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '123456',  
-});
+function createConnection() {
+  return mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123456',  
+  });
+}
 
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
-  console.log('connected as id ' + connection.threadId);
-});
 
-console.log(constants.CHECK_DATABASE_EXIST);
-console.log(constants.CREATE_SOURCE_TABLE);
-console.log(constants.CREATE_LINK_TABLE);
-
-connection.query(constants.CREATE_DATABASE, (error, results, fields) => {
-  if (error) {
-    return;
-  }
-  console.log(results);
-  connection.query(constants.CREATE_SOURCE_TABLE, (error, results, fields) => {
+function checkDBExist(connection = createConnection(), callback) {
+  connection.query(constants.CHECK_DATABASE_EXIST, (error, results, fields) => {
     if (error) {
-      return;
+      connection.end();
+      callback(error, connection);
     }
-    console.log(results);    
-    connection.query(constants.CREATE_LINK_TABLE, (error, results, fields) => {
+    callback(undefined, connection, results);
+  });
+}
+
+function createDB(connection = createConnection(), callback) {
+  connection.query(constants.CREATE_DATABASE, (error, results, fields) => {
+    if (error) {
+      connection.end();
+      callback(error, connection);
+    }
+    callback(undefined, connection, results);
+  })  
+}
+
+checkDBExist(createConnection(), function(error, connection, results) {
+  if (!results || !results.length) {
+    createDB(connection, function (error, connection, results) {
       if (error) {
+        connection.end();
         return;
       }
-      console.log(results);      
-    })
-  });
+      connection.end();
+      return;
+    });
+  }
 })
+
+
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error('error connecting: ' + err.stack);
+//     return;
+//   }
+//   console.log('connected as id ' + connection.threadId);
+// });
+
+// console.log(constants.CHECK_DATABASE_EXIST);
+// console.log(constants.CREATE_SOURCE_TABLE);
+// console.log(constants.CREATE_LINK_TABLE);
+
+// connection.query(constants.CREATE_DATABASE, (error, results, fields) => {
+//   if (error) {
+//     return;
+//   }
+//   console.log(results);
+//   connection.query(constants.CREATE_SOURCE_TABLE, (error, results, fields) => {
+//     if (error) {
+//       return;
+//     }
+//     console.log(results);    
+//     connection.query(constants.CREATE_LINK_TABLE, (error, results, fields) => {
+//       if (error) {
+//         return;
+//       }
+//       console.log(results);      
+//     })
+//   });
+// })
 
