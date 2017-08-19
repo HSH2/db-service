3a7330f68ac5d6e87955666d1a566a34a98df648
liguangyi <252438823@qq.com>
2017-08-19 13:55:59 +0800

    update

--- db.js
+++ db.js
@@ -18,6 +18,15 @@ function commonCallback(connection, callback, error, results, fields) {
   callback(undefined, connection, results, fields);
 }
 
+function createDBConnection() {
+  return mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: constants.DATABASE,
+  });
+}
+
 function checkDBExist(callback, connection = createConnection()) {
   connection.query(constants.CHECK_DATABASE_EXIST, commonCallback.bind(this, connection, callback));
 }
@@ -30,33 +39,26 @@ function dropDB(callback, connection = createConnection()) {
   connection.query(constants.DROP_DATABASE, commonCallback.bind(this, connection, callback));
 }
 
-checkDBExist(function (error, connection, results, fields) {
-  if (error) {
-    console.log(error);
-    connection.end();
-    return;
-  }
-  console.log('HAVE DB--->', results, connection.id);
-  if (results.length) {
-    dropDB(function (error, connection, results, fields) {
-      if (error) {
-        console.log(error);
-        connection.end();
-        return;
-      }
-      console.log('DROP DB--->', results, connection.id);
-      connection.end();
-    }, connection);
-  } else {
-    createDB(function (error, connection, results, fields) {
-      if (error) {
-        console.log(error);
-        connection.end();
-        return;
-      }
-      console.log('CREATE DB--->', results, connection.id);
-      connection.end();
-    }, connection);
-  }
-});
+function createTable(queryStr, callback, connection = createConnection()) {
+  connection.query(queryStr, commonCallback.bind(this, connection, callback));
+}
+
+function createSourceTable(callback, connection = createConnection()) {
+  const queryStr = constants.CREATE_SOURCE_TABLE;
+  createTable(queryStr, callback, connection);
+}
+
+function createLinkTable(callback, connection = createConnection()) {
+  const queryStr = constants.CREATE_LINK_TABLE;
+  createTable(queryStr, callback, connection);
+}
+
+module.exports = {
+  createDBConnection,
+  checkDBExist,
+  createDB,
+  dropDB,
+  createSourceTable,
+  createLinkTable,
+}
 
